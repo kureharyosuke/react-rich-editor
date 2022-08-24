e1f614026b353678f641bd517fca83b92d88857c
kureharyosuke <ohhayo.com@gmail.com>
2022-08-25 00:08:21 +0900

    #4 setup: react-quill setup

---
+++ nextts/react-quill/components/Editor.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactQuill, { Mixin, Toolbar, Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export const Editor = () => {
+  return (
+    <ReactQuill/>
+  )
+}
--- nextts/react-quill/pages/_app.tsx
+++ nextts/react-quill/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import "react-quill/dist/quill.snow.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
