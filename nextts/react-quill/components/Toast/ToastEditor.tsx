b5d08ac3ec7c308600e0398703b575599b35f4bb
kureharyosuke <ohhayo.com@gmail.com>
2022-09-01 21:19:55 +0900

    Toast Editor

---
+++ nextts/react-quill/components/Toast/ToastEditor.tsx
@@ -0,0 +1,80 @@
+import dynamic from 'next/dynamic';
+import React, { useRef, useCallback, forwardRef } from 'react';
+import { Editor as EditorType, EditorProps } from '@toast-ui/react-editor';
+import { TuiEditorWithForwardedProps } from './TUIEditorWrapper';
+
+interface IEditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+interface ITUIEditorProps extends EditorProps {
+  onChange(value: string): void;
+}
+
+const Editor = dynamic<TuiEditorWithForwardedProps>(
+  async () => {
+    const [mod] = await Promise.all([
+      import('./TUIEditorWrapper'),
+      // import('@toast-ui/editor/dist/i18n/ko-kr'), // to implement Korean
+    ]);
+    return mod.default;
+  },
+  {
+    loading: () => <div>loading</div>,
+    ssr: false,
+  }
+);
+const EditorWithForwardedRef = React.memo(
+  forwardRef<EditorType | undefined, IEditorPropsWithHandlers>((props, ref) => (
+    <Editor
+      {...props}
+      forwardedRef={ref as React.MutableRefObject<EditorType>}
+    />
+  ))
+);
+
+const TUIEditor = ({ onChange, ...props }: ITUIEditorProps) => {
+  const { initialValue, height } = props;
+
+  const editorRef = useRef<EditorType>();
+  const handleChange = useCallback(() => {
+    if (!editorRef.current) {
+      return;
+    }
+
+    onChange(editorRef.current.getInstance().getHTML());
+  }, [editorRef, onChange]);
+
+  return (
+    <div>
+      <EditorWithForwardedRef
+        ref={editorRef}
+        language="ko-KR"
+        previewStyle="tab
+        hideModeSwitch
+        usageStatistics={false}
+        initialValue={initialValue}
+        height={height || '600px'}
+        onChange={handleChange}
+        initialEditType="wysiwyg"
+        toolbarItems={[
+          [
+            'heading',
+            'bold',
+            'italic',
+            'strike',
+            'hr',
+            'ul',
+            'ol',
+            'indent',
+            'outdent',
+            'link',
+          ],
+        ]}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default React.memo(TUIEditor);
